49f3f07fa1816cd96e9a68025257fd602db2dd5b
Michael <siminskim0351@gmail.com>
2025-01-13 14:21:13 -0500

    all up to date

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let footer = document.createElement('footer')
+let body = document.querySelector('body')
+let copyright = document.createElement('p')
+// time and date variables
+let today = new Date()
+console.log(today)
+let thisYear = today.getFullYear()
+console.log(thisYear)
+
+// document.body.appendChild(footer).textContent('hello')
+// document.querySelector('footer').textContent = 'hello'
+body.appendChild(footer).textContent = 'hello'
