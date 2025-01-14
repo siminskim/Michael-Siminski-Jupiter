a985514ed380c0cf483e864b90291f9933e83f67
Michael <siminskim0351@gmail.com>
2025-01-14 13:32:30 -0500

    copyright and ul added

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
-let footer = document.createElement('footer')
-let body = document.querySelector('body')
-let copyright = document.createElement('p')
-// time and date variables
-let today = new Date()
-console.log(today)
-let thisYear = today.getFullYear()
-console.log(thisYear)
-let a = 'something'
-let b = 'something'
 
-// document.body.appendChild(footer).textContent('hello')
-// document.querySelector('footer').textContent = 'hello'
-body.appendChild(footer).textContent = 'hello'
+// creating footer element to be appended to body
+let footer = document.createElement("footer");
+// put body element in a variable
+let body = document.querySelector("body");
+let copyright = document.createElement("p");
+// time and date variables
+let today = new Date();
+let thisYear = today.getFullYear();
+let wholeName = "Michael Siminski";
+copyright.innerHTML = `${wholeName} &copy ${thisYear}`;
+body.appendChild(footer);
+footer.appendChild(copyright);                                                                                                                       
+// unorder list
+let skills = ["HTML", "CSS", "GitHub", "Affinity Design", "JavaScript"];
+let skillsList = document.getElementById('skillsList')
+console.log(skillsList)
+
+for(let i = 0; i <= skills.length - 1; i++){
+    // console.log(skills[i])
+    let listItem = document.createElement('li')
+    listItem.innerText = skills[i]
+    skillsList.appendChild(listItem)
+}
+
